9f65f672a0fb7f178a2d91428c60087bc1ced3b9
zergreen <kawhao101@gmail.com>
2023-05-17 18:03:18 +0700

    fix: add profile after create user

--- insert-register.php
+++ insert-register.php
@@ -5,7 +5,13 @@ $password = $_POST['password'];
 
 $sql = "INSERT INTO users (username, password) VALUES ('$username','$password')";
 
-echo $sql;
+$sql2 = "INSERT INTO user_profiles (fullname,email,phone,mobile,address,quote,img)
+        VALUES ('null','null@null','(089)','(123)','bangkok','fuck','0')";
+
+echo $sql2;
 
 $conn->query($sql);
+$conn->query($sql2);
+
+header("Location: login.html");
 ?>
