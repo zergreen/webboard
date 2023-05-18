b0277e003e8cea812b104185c7d3403ef49863ba
Ubuntu <ubuntu@ip-172-31-21-10.ap-southeast-1.compute.internal>
2023-05-18 06:04:11 +0000

    add: register to login

--- kratoo.php
+++ kratoo.php
@@ -19,7 +19,7 @@ $rowTopic = mysqli_fetch_array($resultTopic);
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="./bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css">
 
-    <?php header("Content-Security-Policy: style-src 'unsafe-inline' 'self'; default-src 'self'; ") ?> 
+    <?php //header("Content-Security-Policy: style-src 'unsafe-inline' 'self'; default-src 'self'; ") ?> 
     <title>Kratoo</title>
 </head>
 
